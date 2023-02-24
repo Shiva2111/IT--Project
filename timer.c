5261d526e2287f9801d4aa6864fab84449c47802
Shiv <shivchavda11@gmail.com>
2023-02-24 21:37:59 +0530

    main

--- timer.c
+++ timer.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include<conio.h>
-#include"sound.h"
-int main()
+#include<windows.h>
+#include<time.h>
+void timer()
 {
     int h=0,m=0,s=0,ms=0;
-    int th,tm,ts,tms,t=1;
+    int th,tm,ts,t=1;
         while(t==1)
         {
             printf("Enter Hours:");
@@ -13,6 +14,21 @@ int main()
             scanf("%d",&tm);
             printf("Enter Seconds:");
             scanf("%d",&ts);
+             if (ts>=60)
+        {
+            ts=ts-60;
+            tm=tm+1;
+        }
+        
+        if (tm>=60)
+        {
+            tm=tm-60;
+            th=th+1;
+        }
+        if(th>=24)
+        {
+            th = th - 24;
+        }
             t--;
         }
     while (1)
@@ -21,30 +37,51 @@ int main()
         system("cls");
        s++;
        Beep(5000, 2);
-        if (s==60)
+        if (s>=60)
         {
-            s=0;
+            s=s-60;
             m=m+1;
         }
-        
-        if (m==60)
+        if(s<0)
         {
-            m=0;
+            s=s+60;
+            m=m-1;
+        }
+        if (m>=60)
+        {
+            m=m-60;
             h=h+1;
+        }
+        if(m<0)
+        {
+            m=m+60;
+            h=h-1;
         }
         if(h>=24)
         {
             h = h - 24;
         }
+        else if(h<0)
+        {
+            h=h+24;
+        }
         printf("<--------------------------------------------------------------------Time :  ");  
-        printf("%d : %d : %d--------------------------------------------------------------------->",h,m,s);
+        printf("%d : %d : %d-------------------------------------------------------------------->",h,m,s);
         if (h==th && m==tm && s==ts)
         {
-            printf("\nPress Ctrl+C to stop the timer...");
+        
+            printf("\nPress any key to stop the timer...");
+            
             delay(1);
             sd();
-            return 0;
+            
+            return;
+            
+
+
         }
+            
+        
         
 
     }
