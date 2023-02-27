59f42a3e2c7e54c2ca8f2f2a27e5353ca4af4e08
Shiv <shivchavda11@gmail.com>
2023-02-27 14:10:51 +0530

    a

--- sound.h
+++ sound.h
@@ -1,35 +1,30 @@
-#include<stdio.h>
-#include<windows.h>
-#include<conio.h>
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
 
-void sd(){
-    // Beep(5000, 2);
-    while(1){
-     
-    while(1){
-        
-        Beep(5000, 2);
-        Beep(5000, 2);
-        Beep(5000, 2);
-        Beep(5000, 2);
-        delay(1);
-        Beep(5000, 2);
-        Beep(5000, 2);
-        Beep(5000, 2);
-        Beep(5000, 2);   
-        delay(1);
-        if(kbhit()){
-            // break;
-            return;
+void sd()
+{
+
+    while (1)
+    {
+
+        while (1)
+        {
+
+            Beep(5000, 2);
+            Beep(5000, 2);
+            Beep(5000, 2);
+            Beep(5000, 2);
+            delay(1);
+            Beep(5000, 2);
+            Beep(5000, 2);
+            Beep(5000, 2);
+            Beep(5000, 2);
+            delay(1);
+            if (kbhit())
+            {
+                return;
+            }
         }
-
     }
-    // if(kbhit()){
-    //         break;
-    //     }
-
-    
-    }
-    
-    
-} 
+}
